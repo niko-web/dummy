d2e7b4491f27e698c5ccdf91b9067ee1cf080177
Nikolay <niksanchetyyy@gmail.com>
2023-07-26 19:37:18 +0300

    update code

--- base/views.py
+++ base/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpRequest
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
 
