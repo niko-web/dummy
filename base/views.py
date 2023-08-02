ff34cb8bafe220e66d699d90e22ad39b719e6efb
Nikolay <niksanchetyyy@gmail.com>
2023-08-03 00:35:31 +0300

    working with variables and loops

--- base/views.py
+++ base/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 # Create your views here.
 
+rooms = [
+    {
+        'id':1, 
+        'name': "Nikolay",
+
+    },
+    {
+        'id':2, 
+        'name': "georgi",
+
+    },
+    {
+        'id':3, 
+        'name': "Petar",
+
+    },
+
+]
+
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'rooms': rooms})
 
 def room(request):
     return render(request, 'room.html')
