3e3ff9de47fe7a8ce5c00acfc55b530219bf9a23
Nikolay <niksanchetyyy@gmail.com>
2023-08-29 17:36:49 +0300

    add onetomany relations between user and cars

--- base/models.py
+++ base/models.py
@@ -5,6 +5,10 @@ class User(models.Model):
     username = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     age = models.IntegerField(max_length=3)
+    
+    def __str__(self):
+        return self.username
+
 
 
 class Car(models.Model):
@@ -12,4 +16,15 @@ class Car(models.Model):
     model = models.CharField(max_length=255)
     color = models.CharField(max_length=255)
     engine = models.CharField(max_length=255)
+
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        null=True
+    )
+
+    def __str__(self):
+        return self.brand   
+    
+
 
