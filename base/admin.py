dca6a9763d7f592142bc916c8c8fa30e093c8334
Nikolay <niksanchetyyy@gmail.com>
2023-08-29 16:33:24 +0300

    adding car and user Models

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User, Car
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Car)
--- base/models.py
+++ base/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    age = models.IntegerField(max_length=3)
+
+
+class Car(models.Model):
+    brand = models.CharField(max_length=255)
+    model = models.CharField(max_length=255)
+    color = models.CharField(max_length=255)
+    engine = models.CharField(max_length=255)
+
