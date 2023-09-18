82afff7ee1ddaf2a23f30a00ea04d728a58f247d
Todor Markov <todorator@gmail.com>
2023-09-18 19:28:56 +0300

    new

--- base/admin.py
+++ base/admin.py
@@ -3,20 +3,14 @@ from django.contrib.auth.admin import UserAdmin
 
 from .models import SiteUser, Car
 
-# Register your models here.
-class UserAdmin(UserAdmin):
-    list_display = ['id', 'email', 'created_at', 'updated_at']
 
 class CarAdmin(admin.ModelAdmin):
     list_display = ["id", "brand", "model", "color", "year", "created_at", "updated_at"]
 
-class SiteUserAdmin(admin.ModelAdmin):
-    list_display = ["id", "username", "email", "age", "is_superuser", "date_joined"]
+class SiteUserAdmin(UserAdmin):
+    pass
 
 
 admin.site.register(SiteUser, SiteUserAdmin)
 admin.site.register(Car, CarAdmin)
 
-
-# class CarAdmin(admin.ModelAdmin):
-#     list_display = ["id", "brand", "model"]
