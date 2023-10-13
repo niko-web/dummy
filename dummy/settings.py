dc5a23dd07f8f3e6b00c43792fb4a26e4177a912
Nikolay <niksanchetyyy@gmail.com>
2023-10-13 22:04:49 +0300

    update settings to link css file

--- dummy/settings.py
+++ dummy/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = os.path.join(BASE_DIR, "staticroot/")
 
 
 # Quick-start development settings - unsuitable for production
@@ -119,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "base" / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
